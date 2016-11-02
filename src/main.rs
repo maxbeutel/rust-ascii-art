670c35b979e4bef3158b53e4ef75d31b14c5b3d4
Max Beutel <me@maxbeutel.de>
2016-11-02 12:10:53 +0800

    introduce plottable, but only canvas can be drawn

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,63 @@ struct Dimension(u32, u32);
 #[derive(PartialEq, PartialOrd, Debug, Hash, Eq, Clone)]
 struct Coordinate(u32, u32);
 
-type ShapeCoordinates = HashMap<Coordinate, Shape>;
+impl Coordinate {
+    // @FIXME make this private and move to canvas???
+    fn from_canvas_index(i: u32, canvas_size: &Dimension) -> Coordinate {
+        if i < canvas_size.0 { Coordinate(i, 0) }
+        else { Coordinate(i % canvas_size.0, i / canvas_size.0) }
+    }
+}
+
+// only type alias for now, otherwise I need to re-implement all the functions of the HashMap that I use
+// should be wrapper struct though
+type ShapeCoordinates = HashMap<Coordinate, Shape>; // @FIXME this can soon be a HashSet, because the types know their Shape
+
+trait Plottable {
+    fn shape(&self) -> Shape;
+
+    fn coords(&self) -> &ShapeCoordinates;
+}
 
 #[derive(Debug)]
 struct Canvas(Dimension, ShapeCoordinates);
+
+impl Plottable for Canvas {
+    fn shape(&self) -> Shape {
+        Shape::Canvas
+    }
+
+    fn coords(&self) -> &ShapeCoordinates {
+        &self.1
+    }
+}
+
+#[derive(Debug)]
+struct Circle(ShapeCoordinates);
+
+impl Plottable for Circle {
+    fn shape(&self) -> Shape {
+        Shape::Circle
+    }
+
+    fn coords(&self) -> &ShapeCoordinates {
+        &self.0
+    }
+}
+
+#[derive(Debug)]
+struct Line(ShapeCoordinates); // @FIXME only one line type for now during this refactoring
+
+impl Plottable for Line {
+    fn shape(&self) -> Shape {
+        Shape::HorizontalLine // @TODO return line type here
+    }
+
+    fn coords(&self) -> &ShapeCoordinates {
+        &self.0
+    }
+}
+
 
 #[derive(Clone, Copy, Debug)]
 enum Shape {
@@ -22,14 +75,6 @@ enum Shape {
     VerticalLine,
     DiagonalLineLeftToRight,
     DiagonalLineRightToLeft,
-}
-
-impl Coordinate {
-    // @FIXME make this private and move to canvas???
-    fn from_canvas_index(i: u32, canvas_size: &Dimension) -> Coordinate {
-        if i < canvas_size.0 { Coordinate(i, 0) }
-        else { Coordinate(i % canvas_size.0, i / canvas_size.0) }
-    }
 }
 
 impl Shape {
@@ -45,13 +90,12 @@ impl Shape {
     }
 }
 
+// -- functions --
 fn canvas_dimensions(a: &ShapeCoordinates, b: &ShapeCoordinates) -> Dimension {
     let combined = a.into_iter()
         .chain(b.into_iter())
         .map(|(c, _)| c)
         .collect::<Vec<(&Coordinate)>>();
-
-    //println!("combined: {:?}", combined);
 
     let coord_max_x = combined.iter().max_by_key(|&coords| coords.0).unwrap();
     let coord_max_y = combined.iter().max_by_key(|&coords| coords.1).unwrap();
@@ -59,41 +103,26 @@ fn canvas_dimensions(a: &ShapeCoordinates, b: &ShapeCoordinates) -> Dimension {
     Dimension(coord_max_x.0 + 1, coord_max_y.1 + 1)
 }
 
-fn combine(a: ShapeCoordinates, b: ShapeCoordinates) -> Canvas {
-    let canvas_dimension = canvas_dimensions(&a, &b);
-    let canvas = canvas(&canvas_dimension);
+fn plot_on_canvas<T: Plottable, U: Plottable>(a: &T, b: &U) -> Canvas {
+    let canvas_dimension = canvas_dimensions(a.coords(), b.coords());
+    let canvas = new_canvas(&canvas_dimension);
 
-    // let coords = a.into_iter().chain(b.into_iter()).chain(canvas.into_iter()).collect();
+    // @FIXME: this is quite ugly, bu the new hashmap needs to have ownership of the values from the old one
+    let coords = canvas.iter().map(|a| (a.0.clone(), a.1.clone()))
+        .chain(a.coords().iter().map(|a| (a.0.clone(), a.1.clone())))
+        .chain(b.coords().iter().map(|a| (a.0.clone(), a.1.clone())))
+        .collect();
 
-    let coords = canvas.into_iter().chain(a.into_iter()).chain(b.into_iter()).collect();
-    //println!("dim {:?}", canvas_dimension);
     Canvas(canvas_dimension, coords)
 }
 
-// let a_max_coord_by_x = (a.iter().max_by_key(|&(coords, _)| coords.0).unwrap().0).0;
-// let a_max_coord_by_y = (a.iter().max_by_key(|&(coords, _)| coords.1).unwrap().0).1;
-
-// let b_max_coord_by_x = (b.iter().max_by_key(|&(coords, _)| coords.0).unwrap().0).0;
-// let b_max_coord_by_y = (b.iter().max_by_key(|&(coords, _)| coords.1).unwrap().0).1;
-
-// println!("a_max = {:?}", (a_max_coord_by_x, a_max_coord_by_y));
-// println!("b_max = {:?}", (b_max_coord_by_x, b_max_coord_by_y));
-
-
-// let canvas_dimension_x = [a_max_coord_by_x, b_max_coord_by_x].into_iter().max();
-// let canvas_dimension_y = 0;
-
-//    a.into_iter().chain(b.into_iter()).collect()
-
-fn canvas(size: &Dimension) -> ShapeCoordinates {
-    // println!("creating {} coords", size.0 * size.1);
-
+fn new_canvas(size: &Dimension) -> ShapeCoordinates {
     (0..(size.0 * size.1))
         .map(|i| { (Coordinate::from_canvas_index(i, &size), Shape::Canvas) })
         .collect()
 }
 
-fn circle(radius: u32, point: &Point) -> ShapeCoordinates {
+fn circle(radius: u32, point: &Point) -> Circle {
     let &Point(x0, y0) = point;
 
     let mut x = radius;
@@ -123,7 +152,7 @@ fn circle(radius: u32, point: &Point) -> ShapeCoordinates {
         }
     }
 
-    coords
+    Circle(coords)
 }
 
 fn line_shape(start: &Point, end: &Point) -> Shape {
@@ -141,7 +170,7 @@ fn line_shape(start: &Point, end: &Point) -> Shape {
     }
 }
 
-fn line(start: &Point, end: &Point) -> ShapeCoordinates {
+fn line(start: &Point, end: &Point) -> Line {
     // how to make this nicer and use tuple deconstruction?
     let x0 = start.0 as i32;
     let y0 = start.1 as i32;
@@ -185,7 +214,7 @@ fn line(start: &Point, end: &Point) -> ShapeCoordinates {
         }
     }
 
-    coords
+    Line(coords)
 }
 
 fn draw(canvas: &Canvas, write_fn: &Fn(&Coordinate, char, u32)) {
@@ -213,15 +242,8 @@ fn write_debug(coords: &Coordinate, chr: char, line_length: u32) {
     else { print!("{} ({}/{}) ", chr, coords.0, coords.1); }
 }
 
-// fn plot(canvas_dimension: Dimension, coords: ShapeCoordinates) -> Canvas {
-//     let all_coords_on_canvas = canvas(&canvas_dimension);
-//     let plotted_coords = combine(all_coords_on_canvas, coords);
-//     Canvas(canvas_dimension, plotted_coords)
-// }
-
 fn main() {
-    let num = 10;
-    let canvas_dimension = Dimension(3, 9);
+    let canvas_dimension = Dimension(10, 10);
 
     let point_1 = Point(2, 2);
     let point_2 = Point(3, 4);
@@ -230,10 +252,9 @@ fn main() {
     let line_start = Point(0, 0);
     let line_end = Point(0, 9);
 
-    let mut canvas = combine(circle(1, &point_1), line(&line_start, &line_end));
-    canvas = combine(circle(1, &point_2), canvas);
-    // coords = ;
-    // coords = ;
+    let mut canvas = plot_on_canvas(&circle(1, &point_1), &line(&line_start, &line_end));
+    canvas = plot_on_canvas(&canvas, &circle(1, &point_2));
+    canvas = plot_on_canvas(&canvas, &circle(1, &point_3));
 
     draw(&canvas , &write);
 }
